28d3e527cb685ecd1d69ff33a034cb22ab69a5c0
prasanna kothoju <134475079+prasannakothoju@users.noreply.github.com>
2025-04-14 20:16:45 +0530

    Create countdigits.java

---
+++ countdigits.java
@@ -0,0 +1,29 @@
+//counting digits in a number
+
+import java.util.*;
+import java.util.Scanner;
+
+public class Main{
+    
+    //creating a function for counting digits
+    public static int countDigits(int n){
+        int count =0; //counter for counting digits
+        while(n>0){
+            int lastdigit= n%10;  //for every extraction counter will be incremented
+            count = count+1;
+            n = n/10;
+            
+        }
+        return count;
+    }
+    
+    //main function for printing digits
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt();
+       
+       int res = countDigits(a);
+       System.out.println("no of digits:"+res);
+    }
+}
+
